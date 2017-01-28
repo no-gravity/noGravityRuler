a697c72b93e01e6ef790a8c8aae154e385b8520d
no-gravity <m@gibney.de>
2017-01-28 21:44:09 +0100

    Update noGravityRuler.js

--- noGravityRuler.js
+++ noGravityRuler.js
@@ -1,10 +1,10 @@
 style=`#noGravityRuler {
-position: absolute;
-top: 10px;
-left: 0px;
-background: red;
-width: 100%;
-height: 1px;
+ position: absolute;
+ top: 10px;
+ left: 0px;
+ background: red;
+ width: 100%;
+ height: 1px;
 }`;
 
 s=document.createElement("style");
@@ -16,9 +16,13 @@ noGravityRuler.id="noGravityRuler";
 
 document.body.appendChild(noGravityRuler);
 
-function noGravityRulerMove(e)
-{
+function noGravityRulerMove(e) {
  noGravityRuler.style.top=e.clientY+"px";
 }
 
+function noGravityRulerClick(e) {
+  document.body.removeChild(noGravityRuler);
+}
+
 document.onmousemove=noGravityRulerMove;
+noGravityRuler.onclick=noGravityRulerClick;
